603bccc02d1a472c1a4221d9e570f65a1d71bf9a
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-04 16:27:35 +0530

    inherited model implemented !!!

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,21 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'));
+        home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var color1 = Colors.yellow;
+  var color2 = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +39,33 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: GestureDetector(
         onTap: () async {},
-        child: Center(
-          child: Container(),
+        child: AvailableColorsWidget(
+          color1: color1,
+          color2: color2,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          color1 = allColors.getRandomElement();
+                        });
+                      },
+                      child: const Text("change color 1")),
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          color2 = allColors.getRandomElement();
+                        });
+                      },
+                      child: const Text("change color 2")),
+                ],
+              ),
+              const ColorWidget(colors: AvailableColors.one),
+              const ColorWidget(colors: AvailableColors.two)
+            ],
+          ),
         ),
       ),
     );
@@ -62,4 +88,80 @@ final allColors = [
 
 extension RandomElement<T> on Iterable<T> {
   T getRandomElement() => elementAt(Random().nextInt(length));
+}
+
+class AvailableColorsWidget extends InheritedModel<AvailableColors> {
+  final MaterialColor color1;
+  final MaterialColor color2;
+
+  const AvailableColorsWidget({
+    Key? key,
+    required Widget child,
+    required this.color1,
+    required this.color2,
+  }) : super(child: child, key: key);
+
+  @override
+  bool updateShouldNotify(covariant AvailableColorsWidget oldWidget) {
+    /// if it returns true inherited model will rebuild the widget but if it returns false then flutter will say
+    /// nothing changed and widget will not rebuilt
+    debugPrint("update should notify");
+    return color1 != oldWidget.color1 || color2 != oldWidget.color2;
+  }
+
+  /// it says what is going to change in our data
+  /// it comes in updateShouldNotifyDependent after updateShouldNotify returns true
+  @override
+  bool updateShouldNotifyDependent(
+    covariant AvailableColorsWidget oldWidget,
+    Set<AvailableColors> dependencies,
+  ) {
+    debugPrint("updateShouldNotifyDependent");
+    if (dependencies.contains(AvailableColors.one) &&
+        color1 != oldWidget.color1) {
+      return true;
+    }
+
+    if (dependencies.contains(AvailableColors.two) &&
+        color2 != oldWidget.color2) {
+      return true;
+    }
+
+    return false;
+  }
+
+  static AvailableColorsWidget? of(
+      BuildContext context, AvailableColors aspect) {
+    return InheritedModel.inheritFrom<AvailableColorsWidget>(
+      context,
+      aspect: aspect,
+    );
+  }
+}
+
+class ColorWidget extends StatelessWidget {
+  final AvailableColors colors;
+
+  const ColorWidget({
+    Key? key,
+    required this.colors,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (colors) {
+      case AvailableColors.one:
+        debugPrint("Color1 widget got rebuilt");
+        break;
+      case AvailableColors.two:
+        debugPrint("Color2 widget got rebuilt");
+        break;
+    }
+    final provider = AvailableColorsWidget.of(context, colors);
+    return Container(
+      height: 100,
+      color:
+          colors == AvailableColors.one ? provider?.color1 : provider?.color2,
+    );
+  }
 }
