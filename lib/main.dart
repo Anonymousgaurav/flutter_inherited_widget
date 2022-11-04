757f445ccc069860c6dc4ddb3d9d98c7ef8d8ac3
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-04 16:35:36 +0530

    more examples added inherited model !!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_inherited_widget/logo_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage());
+        home: const InheritedModelExample());
   }
 }
 
