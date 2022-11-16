58c1f5bdfae49cd4a39cda9985fc390664f00392
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-11-16 12:30:30 +0530

    provider code added (watch,select,read)

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,209 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: DateTimeProvider(
-          api: Api(), child: const MyHomePage(title: 'Flutter Demo Home Page')),
+    return ChangeNotifierProvider(
+      create: (_) => ObjectProvider(),
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MyHomePage()),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  ValueKey _textKey = const ValueKey<String>("");
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(DateTimeProvider.of(context)?.api.dateAndTime ?? ""),
+        title: const Text("Home Page"),
       ),
-      body: GestureDetector(
-        onTap: () async {
-          final api = DateTimeProvider.of(context)?.api;
-          final dateTime = await api?._getDateAndTime();
-          setState(() {
-            _textKey = ValueKey(dateTime);
-          });
-        },
-        child: Center(
-          child: Container(
-            child: DateTimeWidget(),
+      body: Column(
+        children: [
+          Row(
+            children: const [
+              Expanded(
+                child: CheapWidget(),
+              ),
+              Expanded(
+                child: ExpensiveWidget(),
+              )
+            ],
           ),
-        ),
+          Row(
+            children: const [
+              Expanded(
+                child: ObjectProviderWidget(),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    context.read<ObjectProvider>().stop();
+                  },
+                  child: const Text("Stop")),
+              TextButton(
+                  onPressed: () {
+                    context.read<ObjectProvider>().start();
+                  },
+                  child: const Text("Start")),
+            ],
+          )
+        ],
       ),
     );
   }
 }
 
-class Api {
-  String? dateAndTime;
+@immutable
+class BaseObject {
+  final String id;
+  final String lastUpdated;
 
-  Future<String> _getDateAndTime() {
-    return Future.delayed(
-            const Duration(seconds: 1), () => DateTime.now().toIso8601String())
-        .then((value) {
-      dateAndTime = value;
-      return value;
-    });
-  }
-}
-
-class DateTimeProvider extends InheritedWidget {
-  final Api api;
-  final String uuid;
-
-  DateTimeProvider({
-    Key? key,
-    required Widget child,
-    required this.api,
-  })  : uuid = const Uuid().v4(),
-        super(key: key, child: child);
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
 
   @override
-  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+  bool operator ==(covariant BaseObject other) => id == other.id;
 
-  static DateTimeProvider? of(BuildContext context) {
-    return context.findAncestorWidgetOfExactType();
+  @override
+  int get hashCode => id.hashCode;
+}
+
+@immutable
+class ExpensiveObject extends BaseObject {}
+
+@immutable
+class CheapObject extends BaseObject {}
+
+class ObjectProvider extends ChangeNotifier {
+  late String id;
+  late CheapObject _cheapObject;
+  late ExpensiveObject _expensiveObject;
+
+  /// When you listen on a [Stream] using [Stream.listen], a [StreamSubscription] object is returned. The subscription provides events
+  /// to the listener,and holds the callbacks used to handle the events. The subscription can also be used to unsubscribe from
+  /// the events, or to temporarily pause the events from the stream.
+  late StreamSubscription _cheapObjectStreamSubscription;
+  late StreamSubscription _expensiveObjectStreamSubscription;
+
+  ObjectProvider()
+      : id = const Uuid().v4(),
+        _cheapObject = CheapObject(),
+        _expensiveObject = ExpensiveObject() {
+    start();
+  }
+
+  @override
+  void notifyListeners() {
+    id = const Uuid().v4();
+    super.notifyListeners();
+  }
+
+  CheapObject get cheapObject => _cheapObject;
+
+  ExpensiveObject get expensiveObject => _expensiveObject;
+
+  /// creating a function that kicks in the stream
+
+  void start() {
+    _cheapObjectStreamSubscription =
+        Stream.periodic(const Duration(seconds: 1)).listen((_) {
+      _cheapObject = CheapObject();
+      notifyListeners();
+    });
+
+    _expensiveObjectStreamSubscription =
+        Stream.periodic(const Duration(seconds: 10)).listen((_) {
+      _expensiveObject = ExpensiveObject();
+      notifyListeners();
+    });
+  }
+
+  void stop() {
+    _cheapObjectStreamSubscription.cancel();
+    _expensiveObjectStreamSubscription.cancel();
   }
 }
 
-class DateTimeWidget extends StatelessWidget {
-  const DateTimeWidget({Key? key}) : super(key: key);
+class CheapWidget extends StatelessWidget {
+  const CheapWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final api = DateTimeProvider.of(context)?.api;
-    return Text(api?.dateAndTime ?? "Tap on screen to fetch date and time");
+    final cheapWidget = context.select<ObjectProvider, CheapObject>(
+        (provider) => provider.cheapObject);
+    return Container(
+      height: 100.0,
+      color: Colors.blue,
+      child: Column(
+        children: [
+          const Text("Expensive Widget"),
+          const Text("Last Updated Widget"),
+          Text(cheapWidget.lastUpdated),
+        ],
+      ),
+    );
+  }
+}
+
+class ExpensiveWidget extends StatelessWidget {
+  const ExpensiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final expensiveWidget = context.select<ObjectProvider, ExpensiveObject>(
+        (provider) => provider.expensiveObject);
+    return Container(
+      height: 100.0,
+      color: Colors.blue,
+      child: Column(
+        children: [
+          const Text("Expensive Widget"),
+          const Text("Last Updated Widget"),
+          Text(expensiveWidget.lastUpdated),
+        ],
+      ),
+    );
+  }
+}
+
+class ObjectProviderWidget extends StatelessWidget {
+  const ObjectProviderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<ObjectProvider>();
+    return Container(
+      height: 100.0,
+      color: Colors.purple,
+      child: Column(
+        children: [
+          const Text("Object Provider Widget"),
+          const Text('ID'),
+          Text(provider.id),
+        ],
+      ),
+    );
   }
 }
